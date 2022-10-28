cae21822f703d17812b54d9c64a870be010f4646
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-10-29 00:30:41 +0200

    add auth middleware that checks user's jwt

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -8,3 +8,31 @@ export const createJWT = (user) => {
     );
     return token;
 };
+
+// Auth middleware
+export const protect = (req, res, next) => {
+    const bearer = req.headers.authorization;
+
+    if (!bearer) {
+        res.status(401);
+        res.json({ message: 'not authorized' });
+        return;
+    }
+
+    const [, token] = bearer.split(' ');
+    if (!token) {
+        res.status(401);
+        res.json({ message: 'not valid token' });
+        return;
+    }
+
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = user;
+        next();
+    } catch (e) {
+        res.status(401);
+        res.json({ message: 'not valid token' });
+        return;
+    }
+};
