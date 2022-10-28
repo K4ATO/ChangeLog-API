773f4170de4ea04fcd32aa435bc136df3386e17e
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-10-29 00:14:28 +0200

    add JWT creation

---
+++ src/modules/auth.ts
@@ -0,0 +1,10 @@
+import jwt from 'jsonwebtoken';
+
+// creating Json Web Tokens
+export const createJWT = (user) => {
+    const token = jwt.sign(
+        { id: user.id, username: user.username },
+        process.env.JWT_SECRET
+    );
+    return token;
+};
