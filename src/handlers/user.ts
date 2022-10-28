0db4823dd49dcae01b52f3301b1ddc8124b91d11
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-10-29 01:08:20 +0200

    add sign in middleware

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,6 +1,7 @@
-import { createJWT, hashPassword } from '../modules/auth';
+import { comparePasswords, createJWT, hashPassword } from '../modules/auth';
 import prisma from '../modules/db';
 
+// stores user info in database, creates user's jwt and sends it.
 export const createNewUser = async (req, res) => {
     const user = await prisma.user.create({
         data: {
@@ -8,6 +9,25 @@ export const createNewUser = async (req, res) => {
             password: await hashPassword(req.body.password),
         },
     });
+
+    const token = createJWT(user);
+    res.json({ token });
+};
+
+// finds user in database by username then checks if entered password matches the stored hashed one.
+export const signIn = async (req, res) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            username: req.body.username,
+        },
+    });
+
+    const isValid = await comparePasswords(req.body.password, user.password);
+    if (!isValid) {
+        res.status(401);
+        res.json({ message: 'not valid username or password' });
+        return;
+    }
 
     const token = createJWT(user);
     res.json({ token });
