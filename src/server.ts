7eb0ae2b2fb8960450f508e2dc969e16922c483b
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-10-28 23:00:45 +0200

    add routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
-
+import router from './router';
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log('hello from express');
-    res.status(200);
-    res.json({ message: 'hello' });
-});
+// every route will be /api/whatever_the_route
+// ex: get /api/product:id
+app.use('/api', router);
 
 export default app;
