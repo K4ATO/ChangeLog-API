7fbbcaecaa96594766808ef1dd3c47340e317795
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-10-29 11:06:04 +0200

    add input handler to product routes

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,13 +1,24 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
+import { handleInputErrors } from '../modules/handleInputErrors';
 
 const router = Router();
 
 router.get('/product', (req, res) => {
     res.json({ message: 'from product' });
 });
+
 router.get('/product/:id', () => {});
-router.post('/product', () => {});
-router.put('/product/:id', () => {});
+
+router.post('/product', body('name').isString(), handleInputErrors, () => {});
+
+router.put(
+    '/product/:id',
+    body('name').isString(),
+    handleInputErrors,
+    (req, res) => {}
+);
+
 router.delete('/product/:id', () => {});
 
 export default router;
