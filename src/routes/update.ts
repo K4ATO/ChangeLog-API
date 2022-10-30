9b591c3597f43427abde7d3ba42fea7f3679e87e
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-10-30 21:42:17 +0200

    use update handlers in update routes

--- src/routes/update.ts
+++ src/routes/update.ts
@@ -1,18 +1,24 @@
 import { Router } from 'express';
 import { body, oneOf } from 'express-validator';
+import {
+    createUpdate,
+    deleteUpdate,
+    getUpdate,
+    getUpdates,
+    updateUpdate,
+} from '../handlers/update';
 const router = Router();
 
-router.get('/update', (req, res) => {
-    res.json({ message: 'from update' });
-});
+router.get('/update', getUpdates);
 
-router.get('/update/:id', () => {});
+router.get('/update/:id', getUpdate);
 
 router.post(
     '/update',
     body('title').exists().isString(),
     body('body').exists().isString,
-    () => {}
+    body('productId').exists().isString(),
+    createUpdate
 );
 
 router.put(
@@ -21,9 +27,9 @@ router.put(
     body('body').optional(),
     body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']),
     body('version').optional(),
-    () => {}
+    updateUpdate
 );
 
-router.delete('/update/:id', () => {});
+router.delete('/update/:id', deleteUpdate);
 
 export default router;
