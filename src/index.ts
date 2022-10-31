f5953d568f3b0bd407ecf0ac7b1990777e287bfc
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-10-31 15:29:42 +0200

    add port config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import * as dotenv from 'dotenv';
 import app from './server';
+import config from './config';
 
 dotenv.config();
-app.listen(3001, () => {
-    console.log();
+app.listen(config.port, () => {
+    console.log(`running on http://localhost:${config.port}`);
 });
